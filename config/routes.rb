c476d546844ae8e858efe3503949c6edd02f8223
Niyi <niyi73@gmail.com>
2014-10-16 17:07:40 -0400

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/...'
+  root 'static_pages/home' 
 
-  get 'static_pages/home'
+  get 'static_pages/home' => 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'static_pages/help' => 'static_pages#help'
 
   resources :locations
 
