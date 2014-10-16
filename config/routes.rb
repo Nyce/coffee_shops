56c845ebd42c4a7b151e8e6bbf2384922727a6b3
Niyi <niyi73@gmail.com>
2014-10-16 17:41:47 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
 
-
+  root 'static_pages#home'
   resources :locations
 
   # The priority is based upon order of creation: first created -> highest priority.
